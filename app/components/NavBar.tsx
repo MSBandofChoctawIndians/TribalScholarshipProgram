999a75840b2fed002bea54ef753b0b2d5b4a416d
Sonny Mattera <sonny.mattera@choctaw.org>
2023-10-26 13:29:09 -0500

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import {
@@ -9,7 +9,7 @@ import {
   Link,
   NavbarMenuToggle,
   NavbarMenu,
-  NavbarMenuItem
+  NavbarMenuItem,
 } from "@nextui-org/react";
 import { usePathname } from "next/navigation";
 import NextLink from "next/link";
@@ -29,7 +29,7 @@ export default function NavBar() {
         />
         <NavbarBrand>
           <TSPLogo />
-          <p className="font-bold text-inherit pl-4">TRIBAL SCHOLARSHIP</p>
+          <p className="font-bold text-inherit pl-2">TRIBAL SCHOLARSHIP</p>
         </NavbarBrand>
       </NavbarContent>
 
@@ -37,7 +37,7 @@ export default function NavBar() {
         {navLinks.map((link, index) => (
           <NavbarItem key={`${link}-${index}`}>
             <Link
-              color={`${pathname === link.path ? "primary": "foreground"}`}
+              color={`${pathname === link.path ? "primary" : "foreground"}`}
               href={link.path}
               as={NextLink}
             >
@@ -45,33 +45,13 @@ export default function NavBar() {
             </Link>
           </NavbarItem>
         ))}
-        {/* <NavbarItem>
-          <Link
-            color={`${pathname === "/" ? "primary" : "foreground"}`}
-            href="/"
-            as={NextLink}
-          >
-            Home
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link
-            color={`${pathname === "/about" ? "primary" : "foreground"}`}
-            href="/about"
-            as={NextLink}
-          >
-            About
-          </Link>
-        </NavbarItem> */}
       </NavbarContent>
 
       <NavbarMenu>
         {navLinks.map((link, index) => (
           <NavbarMenuItem key={`${link}-${index}`}>
-            <Link 
-              color={
-                index === 2 ? "primary" : index === navLinks.length - 1 ? "danger" : "foreground"
-              }
+            <Link
+              color={`${pathname === link.path ? "primary" : "foreground"}`}
               className="w-full"
               href={link.path}
               size="lg"
@@ -80,28 +60,6 @@ export default function NavBar() {
             </Link>
           </NavbarMenuItem>
         ))}
-        {/* <NavbarMenuItem>
-          <Link
-            color={`${pathname === "/" ? "primary" : "foreground"}`}
-            className="w-full"
-            href="/"
-            size="lg"
-            as={NextLink}
-          >
-            Home
-          </Link>
-        </NavbarMenuItem>
-        <NavbarMenuItem>
-          <Link
-            color={`${pathname === "/about" ? "primary" : "foreground"}`}
-            className="w-full"
-            href="/about"
-            size="lg"
-            as={NextLink}
-          >
-            About
-          </Link>
-        </NavbarMenuItem> */}
       </NavbarMenu>
     </Navbar>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next"
-import Link from "next/link"
+import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Home | Tribal Scholarship Program',
-  description: 'The Scholarship Program pledges to provide high-quality, pre-college counseling by: helping students explore majors and assisting with admission, housing and financial aid applications and the registration process at nearby colleges; making on-campus visits to monitor student progress and to assist in resolving problems; and providing financial assistance by using the Title IV Cost of Attendance from each college or university to set the scholarship award amount for each individual student.',
-}
+  title: "Home | Tribal Scholarship Program",
+  description:
+    "The Scholarship Program pledges to provide high-quality, pre-college counseling by: helping students explore majors and assisting with admission, housing and financial aid applications and the registration process at nearby colleges; making on-campus visits to monitor student progress and to assist in resolving problems; and providing financial assistance by using the Title IV Cost of Attendance from each college or university to set the scholarship award amount for each individual student.",
+};
 
 // export const videoSources = [
 //   './vid/video01.mp4',
@@ -17,5 +18,5 @@ export default function Page() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
     </div>
-  )
+  );
 }
