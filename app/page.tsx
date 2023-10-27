ad3c5042f429c0c44bc7d726cb706773e9756907
Sonny Mattera <sonny.mattera@choctaw.org>
2023-10-27 08:55:10 -0500

    Install framer-motion-ticker

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
 import type { Metadata } from "next";
 import Link from "next/link";
+import CollegeTicker from "./components/CollegeTicker";
 
 export const metadata: Metadata = {
   title: "Home | Tribal Scholarship Program",
   description:
     "The Scholarship Program pledges to provide high-quality, pre-college counseling by: helping students explore majors and assisting with admission, housing and financial aid applications and the registration process at nearby colleges; making on-campus visits to monitor student progress and to assist in resolving problems; and providing financial assistance by using the Title IV Cost of Attendance from each college or university to set the scholarship award amount for each individual student.",
+  openGraph: {
+    title: "Home | Tribal Scholarship Program",
+    description:
+      "The Scholarship Program pledges to provide high-quality, pre-college counseling by: helping students explore majors and assisting with admission, housing and financial aid applications and the registration process at nearby colleges; making on-campus visits to monitor student progress and to assist in resolving problems; and providing financial assistance by using the Title IV Cost of Attendance from each college or university to set the scholarship award amount for each individual student.",
+    url: "https://scholarship.choctaw.org/",
+    siteName: "Tribal Scholarship Program",
+    images: [
+      {
+        url: "https://nextjs.org/og.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://nextjs.org/og-alt.png",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 // export const videoSources = [
@@ -17,6 +40,7 @@ export default function Page() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <CollegeTicker />
     </div>
   );
 }
