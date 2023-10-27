2af880c25916a7f74e344916c51ee3b960bb0752
Sonny Mattera <sonny.mattera@choctaw.org>
2023-10-27 09:00:15 -0500

    Update CollegeTicker.tsx

--- app/components/CollegeTicker.tsx
+++ app/components/CollegeTicker.tsx
@@ -6,7 +6,8 @@ import Ticker from "framer-motion-ticker";
 function CollegeTicker() {
   const colors = ["#632bf3", "#f122c8", "#f16022", "#9ef344", "#44d3f3"];
   return (
-    <div className="App">
+    <div className="bg-white">
+      <h1 className="text-center text-lg font-bold">Recent Graduates from</h1>
       <Ticker duration={20}>
         {colors.map((item, index) => (
           <div
