f2d2144be52b763915ccf63d505cddfe4b6139a5
Sonny Mattera <sonny.mattera@choctaw.org>
2023-10-27 10:03:22 -0500

    Update bgvid

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Page() {
           muted
           className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
         >
-          <source src="./vid/Tashina_Taylor.mp4" type="video/mp4" />
+          <source src="Tashina_Taylor.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </header>
