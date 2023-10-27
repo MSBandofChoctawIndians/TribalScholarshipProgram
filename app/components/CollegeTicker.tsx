fe754e0593d2991d572ee4849f453a27d7c6f6a6
Sonny Mattera <sonny.mattera@choctaw.org>
2023-10-27 12:20:03 -0500

    Update CollegeTicker.tsx

--- app/components/CollegeTicker.tsx
+++ app/components/CollegeTicker.tsx
@@ -66,7 +66,7 @@ function CollegeTicker() {
               alt={item.name}
               height={200}
               width={200}
-              className="m-6 fill-slate-950 hover:fill-slate-400"
+              className="m-6 scale-100 hover:scale-110"
             />
           </a>
         ))}
