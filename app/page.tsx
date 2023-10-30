6059e9a24efe5da9af318cd45d122d3777671cad
Sonny Mattera <sonny.mattera@choctaw.org>
2023-10-30 08:48:32 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import CollegeTicker from "./components/CollegeTicker";
+import Stats from "./components/Stats";
 
 export const metadata: Metadata = {
   title: "Home | Tribal Scholarship Program",
@@ -48,6 +50,7 @@ export default function Page() {
       </header>
       <div>
         <CollegeTicker />
+        <Stats />
       </div>
     </>
   );
