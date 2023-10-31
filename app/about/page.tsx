c3f1c30ab782ac7b4a7d4728d5f0100bc2c9356e
Sonny Mattera <smattera@hotmail.com>
2023-10-31 11:29:53 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,67 +1,74 @@
 export default function About() {
   return (
-    <div className="mt-8 mx-4 prose prose-slate mx-auto md:prose-lg lg:prose-xl dark:prose-invert">
-      <figure>
-        <img
-          src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
-          alt=""
-        />
-      </figure>
-      <p className="lead">Halito! Welcome to the Tribal Scholarship Program!</p>
-      <p>
-        The Mississippi Band of Choctaw Indians has consistently placed
-        education as a high priority and emphasized the importance of increasing
-        the number of students entering and completing post- secondary
-        educational programs. To this purpose, the Tribal Scholarship Program
-        was established to provide scholarship funds to eligible tribal members
-        who wish to further their education beyond the secondary level.
-      </p>
-      <p>
-        The primary objective of the Tribal Scholarship Program is to provide to
-        eligible members of the Mississippi Band of Choctaw Indians who are
-        committed to furthering their post-secondary education the best possible
-        financial aid package for each student. The program also counsels and
-        assists tribal members in structuring their educational endeavors and
-        serves as a student's advocate at the post-secondary educational
-        institution when necessary.
-      </p>
-      <p>
-        The Tribal Scholarship Program will assist students in deciding a field
-        of study (major) in college as well as what college/university to attend
-        when asked to do so. The Tribal Scholarship Staff will assist in the
-        application process to the program and also assist students in
-        completing the Free Application for Federal Student Aid (FAFSA). FAFSA
-        or Pell Grant applications are done online at www.fafsa.ed.gov. Students
-        should be aware of the Financial Aid Priority deadlines at the colleges
-        and universities each year. Some colleges have an April 1 deadline and
-        will process financial aid requests to those students who have met their
-        deadline first. There are deadlines for applications to be turned in to
-        the Tribal Scholarship Program each school year. Student applications to
-        the Tribal Scholarship Program will be considered complete when all the
-        items on the checklist are in the Tribal scholarship Office by the
-        supporting document deadline. A full-time student's award will be
-        determined when all documents are in.
-      </p>
-      <p>
-        The Tribal Scholarship Program will assist the students in the
-        application process dealing with the colleges /universities. Students
-        need to apply for admission to the college/university of their choice
-        months before the college term begins. It is recommended that new
-        students apply to several colleges/universities in their senior year in
-        high school. Students should also apply for housing (dorm) at the same
-        time they apply for admission. Students must work closely with their
-        College Financial Aid Office as well the Business Office. For full-time
-        students, the College Financial Aid Office will need to send to the
-        Tribal Scholarship Office the Financial Aid Package Form. The College
-        Financial Aid Officewill need the Student Aid Report (SAR) from the
-        completed FAFSA in order to complete the Financial Aid Package Form.
-        Full-time student awards for the Tribal Scholarship Program cannot be
-        processed without the completed Financial Aid Package Form from the
-        college/university. Also, an application is considered incomplete if all
-        the supporting documents are not in the student's file at the Tribal
-        Scholarship Office. An incomplete application or an application received
-        after the deadline will not be processed.
-      </p>
+    <div className="mx-4">
+      <div className="mt-8 mx-4 prose prose-slate mx-auto md:prose-lg lg:prose-xl dark:prose-invert">
+        <figure>
+          <img
+            src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
+            alt=""
+          />
+        </figure>
+        <p className="lead">
+          Halito! Welcome to the Tribal Scholarship Program!
+        </p>
+        <p>
+          The Mississippi Band of Choctaw Indians has consistently placed
+          education as a high priority and emphasized the importance of
+          increasing the number of students entering and completing post-
+          secondary educational programs. To this purpose, the Tribal
+          Scholarship Program was established to provide scholarship funds to
+          eligible tribal members who wish to further their education beyond the
+          secondary level.
+        </p>
+        <p>
+          The primary objective of the Tribal Scholarship Program is to provide
+          to eligible members of the Mississippi Band of Choctaw Indians who are
+          committed to furthering their post-secondary education the best
+          possible financial aid package for each student. The program also
+          counsels and assists tribal members in structuring their educational
+          endeavors and serves as a student's advocate at the post-secondary
+          educational institution when necessary.
+        </p>
+        <p>
+          The Tribal Scholarship Program will assist students in deciding a
+          field of study (major) in college as well as what college/university
+          to attend when asked to do so. The Tribal Scholarship Staff will
+          assist in the application process to the program and also assist
+          students in completing the Free Application for Federal Student Aid
+          (FAFSA). FAFSA or Pell Grant applications are done online at
+          www.fafsa.ed.gov. Students should be aware of the Financial Aid
+          Priority deadlines at the colleges and universities each year. Some
+          colleges have an April 1 deadline and will process financial aid
+          requests to those students who have met their deadline first. There
+          are deadlines for applications to be turned in to the Tribal
+          Scholarship Program each school year. Student applications to the
+          Tribal Scholarship Program will be considered complete when all the
+          items on the checklist are in the Tribal scholarship Office by the
+          supporting document deadline. A full-time student's award will be
+          determined when all documents are in.
+        </p>
+        <p>
+          The Tribal Scholarship Program will assist the students in the
+          application process dealing with the colleges /universities. Students
+          need to apply for admission to the college/university of their choice
+          months before the college term begins. It is recommended that new
+          students apply to several colleges/universities in their senior year
+          in high school. Students should also apply for housing (dorm) at the
+          same time they apply for admission. Students must work closely with
+          their College Financial Aid Office as well the Business Office. For
+          full-time students, the College Financial Aid Office will need to send
+          to the Tribal Scholarship Office the Financial Aid Package Form. The
+          College Financial Aid Officewill need the Student Aid Report (SAR)
+          from the completed FAFSA in order to complete the Financial Aid
+          Package Form. Full-time student awards for the Tribal Scholarship
+          Program cannot be processed without the completed Financial Aid
+          Package Form from the college/university. Also, an application is
+          considered incomplete if all the supporting documents are not in the
+          student's file at the Tribal Scholarship Office. An incomplete
+          application or an application received after the deadline will not be
+          processed.
+        </p>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <header className="relative flex items-center justify-center h-[85vh] mb-12 overflow-hidden">
+      <header className="relative flex items-center justify-center h-[85vh] overflow-hidden">
         <div className="relative z-30 p-5 bg-black bg-opacity-50 rounded-xl self-end mb-5 max-w-[1024px]">
           <h2 className="text-base text-3xl md:text-5xl text-white text-center font-semibold tracking-wide">
             Elevating Indigenous Voices, One Scholarship at a Time
@@ -55,8 +55,59 @@ export default function Page() {
       </header>
       <div>
         <CollegeTicker />
-        <Stats />
       </div>
+      <div className="mx-4">
+        <div className="mt-8 prose prose-slate mx-auto md:prose-lg lg:prose-xl dark:prose-invert">
+          <figure>
+            <img
+              src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
+              alt=""
+            />
+          </figure>
+          <p className="lead">
+            Halito! Welcome to the Tribal Scholarship Program!
+          </p>
+          <p>
+            The Scholarship Program pledges to provide high-quality, pre-college
+            counseling by: helping students explore majors and assisting with
+            admission, housing and financial aid applications and the
+            registration process at nearby colleges; making on-campus visits to
+            monitor student progress and to assist in resolving problems; and
+            providing financial assistance by using the Title IV Cost of
+            Attendance from each college or university to set the scholarship
+            award amount for each individual student.
+          </p>
+          <p>
+            The primary objective of the Tribal Scholarship Program is to
+            provide to eligible members of the Mississippi Band of Choctaw
+            Indians who are committed to furthering their post-secondary
+            education the best possible financial aid package for each student.
+            The program also counsels and assists tribal members in structuring
+            their educational endeavors and serves as a student's advocate at
+            the post-secondary educational institution when necessary.
+          </p>
+          <p>
+            Although most applicants seek financial assistance, this program
+            provides a variety of post-secondary educational services. The
+            scholarship is not need-based; however, students must complete the
+            financial aid application process at the college to document
+            expenses, resources and other financial aid that provide a basis for
+            scholarship awards. This process includes completing the Free
+            Application for Federal Student Aid. Applicants seeking financial
+            assistance must be an enrolled member of the Mississippi Band of
+            Choctaw Indians.
+          </p>
+          <p>
+            Scholarship assistance is available at the bachelor, master and
+            doctorate level, as well as technical degrees and a limited number
+            of vocational programs of study. Students wishing to enter a
+            technical or vocational program must file an application with the
+            Vocational Education Program and Employment Assistance Program prior
+            to receiving funding through the Tribal Scholarship Program.
+          </p>
+        </div>
+      </div>
+      <Stats />
     </>
   );
 }
