0c23e291474e2c3d4601719c4427ec7d568890a4
Sonny Mattera <smattera@hotmail.com>
2023-12-28 10:22:20 -0600

    Add WeaveBand component to Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,32 +1,36 @@
 import { Image } from "@nextui-org/image";
 import Link from "next/link";
+import WeaveBand from "./WeaveBand";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#420020] py-4 text-white">
-      <div className="mx-auto flex max-w-[1024px] flex-col items-center justify-between lg:flex-row">
-        <div className="item-center mb-4 flex gap-4 md:mb-0">
-          <Image
-            src="./image/tsp_logo.svg"
-            alt="Tribal Scholarship Program"
-            width={160}
-            height={160}
-          />
-          <Link href="https://www.choctaw.org/">
+    <>
+      <WeaveBand />
+      <footer className="bg-[#420020] py-4 text-white">
+        <div className="mx-auto flex max-w-[1024px] flex-col items-center justify-between lg:flex-row">
+          <div className="item-center mb-4 flex gap-4 md:mb-0">
             <Image
-              src="./image/seal.webp"
-              alt="Mississippi Band of Choctaw Indians"
+              src="./image/tsp_logo.svg"
+              alt="Tribal Scholarship Program"
               width={160}
               height={160}
             />
-          </Link>
+            <Link href="https://www.choctaw.org/">
+              <Image
+                src="./image/seal.webp"
+                alt="Mississippi Band of Choctaw Indians"
+                width={160}
+                height={160}
+              />
+            </Link>
+          </div>
+          <p className="text-md text-center lg:text-right">
+            This page is maintained by the Office of Public Information.
+            Copyright &copy; {new Date().getFullYear()}. Mississippi Band of
+            Choctaw Indians. All rights reserved.
+          </p>
         </div>
-        <p className="text-md text-center lg:text-right">
-          This page is maintained by the Office of Public Information. Copyright{" "}
-          &copy; {new Date().getFullYear()}. Mississippi Band of Choctaw{" "}
-          Indians. All rights reserved.
-        </p>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
